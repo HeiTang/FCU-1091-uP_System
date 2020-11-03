669d5a9f041fe068ceadb07198f233be7e3a896f
HeiTang <tim881205@gmail.com>
2020-11-03 23:47:42 +0800

    Feat: Lab6

---
+++ Lab6/KEIL/MCU_init.h
@@ -0,0 +1,20 @@
+//Define Clock source
+#define MCU_CLOCK_SOURCE      
+#define MCU_CLOCK_SOURCE_HXT  // HXT, LXT, HIRC, LIRC
+#define MCU_CLOCK_FREQUENCY   50000000  //Hz
+
+//Define MCU Interfaces
+#define MCU_INTERFACE_SPI3
+#define SPI3_CLOCK_SOURCE_HCLK // HCLK, PLL
+#define PIN_SPI3_SS0_PD8
+#define PIN_SPI3_SCLK_PD9
+#define PIN_SPI3_MISO0_PD10
+#define PIN_SPI3_MOSI0_PD11
+
+//Timer0
+#define MCU_INTERFACE_TMR0
+#define TMR0_CLOCK_SOURCE_HXT // HXT, LXT, HCLK, EXT, LIRC, HIRC
+#define TMR0_CLOCK_DIVIDER    1
+
+//PB15
+#define MCU_INTERFACE_INT1_PB15
