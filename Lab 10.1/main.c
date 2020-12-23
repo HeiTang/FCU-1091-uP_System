c577514071d258f1c32f5eefff61e14ba2ef438b
HeiTang <tim881205@gmail.com>
2020-12-24 03:47:33 +0800

    Feat: Lab10.1

--- Lab 10.1/main.c
+++ Lab 10.1/main.c
@@ -1,6 +1,6 @@
 //
 // UART_TX : UART Transmit a string to USB-UART com port
-//                  
+//
 // EVB : Nu-LB-NUC140
 // MCU : NUC140VE3CN (LQFP100)
 // UART: UART0 set at baudrate=115200, databit=8, stopbit=1, paritybit=0, flowcontrol=None
@@ -17,115 +17,196 @@
 #include "NUC100Series.h"
 #include "MCU_init.h"
 #include "SYS_init.h"
-#include "Scankey.h"
-#include "Seven_Segment.h"
-#include <stdlib.h>
 #include "LCD.h"
+#include "Seven_Segment.h"
+#include "scankey.h"
 
-char Text[16];
-char num[4];
+char Text[128];
+
+uint8_t comRbuf[256];
+volatile uint8_t comRbytes = 0;
+volatile uint8_t RX_Flag = 0;
+char key[4] = "8723";
+int input;
+int flag;
+int a = 0, b = 0;
+int Line = 0;
+int digit = 0;
+int digit_temp;
+int clear = 0;
 volatile uint8_t int_cnt;
 
+volatile uint32_t index_5ms, cnt_5ms, cnt_1s, cnt_100ms, count;
+volatile uint32_t digit0, digit1, digit2, digit3;
 
-uint8_t  comRbuf[256];
-volatile uint8_t comRbytes = 0;
-volatile uint8_t RX_Flag =0;
-
-void Display_7seg(uint16_t value)
+void TMR0_IRQHandler(void)
 {
-    uint8_t digit;
-    digit = value / 1000;
-		num[0] = digit;
-    CloseSevenSegment();
-    ShowSevenSegment(3, digit);
-    CLK_SysTickDelay(5000);
+	cnt_5ms++;
+	index_5ms = cnt_5ms % 4;
+	if (index_5ms == 0)
+	{
+		CloseSevenSegment();
+		ShowSevenSegment(0, 3);
+	}
+	if (index_5ms == 1)
+	{
+		CloseSevenSegment();
+		ShowSevenSegment(1, 2);
+	}
+	if (index_5ms == 2)
+	{
+		CloseSevenSegment();
+		ShowSevenSegment(2, 7);
+	}
+	if (index_5ms == 3)
+	{
+		CloseSevenSegment();
+		ShowSevenSegment(3, 8);
+	}
 
-    value = value - digit * 1000;
-    digit = value / 100;
-		num[1] = digit;
-    CloseSevenSegment();
-    ShowSevenSegment(2, digit);
-    CLK_SysTickDelay(5000);
+	if (cnt_5ms % 20 == 0)
+	{
+		cnt_100ms++;
+	}
 
-    value = value - digit * 100;
-    digit = value / 10;
-		num[2] = digit;
-    CloseSevenSegment();
-    ShowSevenSegment(1, digit);
-    CLK_SysTickDelay(5000);
+	if (cnt_5ms % 100 == 0)
+	{
+		//NVIC_EnableIRQ(GPAB_IRQn);
+		//GPIO_EnableInt(PA, 0, GPIO_INT_LOW);
+		//GPIO_EnableInt(PA, 1, GPIO_INT_LOW);
+		//GPIO_EnableInt(PA, 2, GPIO_INT_LOW);
+		int_cnt = 0;
+	}
 
-    value = value - digit * 10;
-    digit = value;
-		num[3] = digit;
-    CloseSevenSegment();
-    ShowSevenSegment(0, digit);
-    CLK_SysTickDelay(5000);
+	if (cnt_5ms % 200 == 0)
+	{
+		cnt_1s++;
+	}
+	TIMER_ClearIntFlag(TIMER0);
+}
+
+void Init_Timer0(void)
+{
+	TIMER_Open(TIMER0, TIMER_PERIODIC_MODE, 200);
+	TIMER_EnableInt(TIMER0);
+	NVIC_EnableIRQ(TMR0_IRQn);
+	TIMER_Start(TIMER0);
+}
+
+void Compare(char guess[])
+{
+	int i, j;
+	char result[6];
+	a = 0, b = 0;
+	for (i = 0; i < 4; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			if (i == j && key[i] == guess[j])
+			{
+				a++;
+			}
+			else if (i != j && key[i] == guess[j])
+			{
+				b++;
+			}
+		}
+	}
+	sprintf(result, "%dA%dB", a, b);
+	printS(80, 16 * Line, result);
 }
 
 void UART02_IRQHandler(void)
 {
-	uint8_t c, i, j, a = 0, b = 0;
+	uint8_t in_char;
+	char guess[5] = "     ";
+	uint8_t c, i;
 	uint32_t u32IntSts = UART0->ISR;
-	
-	if(u32IntSts & UART_IS_RX_READY(UART0)) // check ISR on & RX is ready
-  {
-		while (!(UART0->FSR & UART_FSR_RX_EMPTY_Msk)){ // check RX is not empty
+	//print_Line(0, "UART0-RX");
+	if (u32IntSts & UART_IS_RX_READY(UART0)) // check ISR on & RX is ready
+	{
+		while (!(UART0->FSR & UART_FSR_RX_EMPTY_Msk))
+		{						  // check RX is not empty
 			c = UART_READ(UART0); // read UART RX data
-			if (c!='\n') {        // check line-end 
+			if (c != '\n')
+			{ // check line-end
 				comRbuf[comRbytes] = c;
 				comRbytes++;
-			} 		
-			else {
-				for(i=0 ; i<4 ; i++){
-					for(j=0 ; j<4 ; j++){
-						if(i == j && comRbuf[i] == num[j]) a++;
-						if(i != j && comRbuf[i] == num[j]) b++;
-					}
-				}	
-				sprintf(Text,"%d%d%d%d    %dA%dB",comRbuf[0],comRbuf[1],comRbuf[2],comRbuf[3],a,b);
-				comRbytes=0;
-				RX_Flag=1;	                 // set flag when BT command input
+			}
+			else
+			{
+				for (i = 0; i < comRbytes; i++)
+					Text[i] = comRbuf[i]; // store received data to Message
+				for (i = comRbytes; i < 16; i++)
+					Text[i] = ' ';
+				comRbytes = 0;
+				clear = 1; // set flag when BT command input
 				break;
 			}
-		}		
+		}
+		for (i = 0; i < 4; i++)
+		{
+			guess[i] = Text[i];
+		}
+		if (Line < 4 && clear == 1)
+		{
+			print_Line(Line, guess);
+			Compare(guess);
+			Line++;
+			clear = 0;
+			//UART_ClearIntFlag(UART0 ,1);
+		}
 	}
 }
-	
+
 void Init_UART(void)
 {
-	UART_Open(UART0,115200);
-  UART_ENABLE_INT(UART0, UART_IER_RDA_IEN_Msk); // enable UART0 interrupt (triggerred by Read-Data-Available)
-  NVIC_EnableIRQ(UART02_IRQn);		              // enable Cortex-M0 NVIC interrupt for UART02
+	UART_Open(UART0, 115200);
+	UART_ENABLE_INT(UART0, UART_IER_RDA_IEN_Msk);
+	NVIC_EnableIRQ(UART02_IRQn);
 }
 
 void SerialPrint(char *Text)
 {
-	uint8_t key;
-	key = ScanKey();
-	print_Line(2, key);
-	key = key + 0x30; 
-	UART_WRITE(UART0, key);
-	UART_WRITE(UART0, '\n');
+	UART_Write(UART0, Text, 5);
 }
 
 int32_t main()
 {
-	int number = 1234;
-	uint8_t key;
-  SYS_Init();	
-	init_LCD();   // initialize LCD
-	Init_UART(); // initialize UART1 for Bluetooth
+	int num = 0;
+	char DB[4];
+	SYS_Init();
+	Init_UART();
+	init_LCD();
+	Init_Timer0();
+	clear_LCD();
 	OpenKeyPad();
 	OpenSevenSegment();
-  clear_LCD();  // clear LCD screen	
 
-  while(1) {
-		Display_7seg(number);
-		if (RX_Flag == 1) {
-			print_Line(2, Text);
-			RX_Flag=0;
+	while (1)
+	{
+		input = ScanKey();
+		if (input != 0 && flag == 0)
+		{
+			if (digit < 4)
+			{
+				num = num * 10 + input;
+				digit++;
+			}
+			flag = 1;
 		}
-	  SerialPrint(Text);
-    CLK_SysTickDelay(1000);
-  }
+		else if (input == 0)
+		{
+			flag = 0;
+		}
+		if (digit == 4)
+		{
+			sprintf(Text, "%d\n", num);
+			SerialPrint(Text);
+			CLK_SysTickDelay(1000);
+			digit = 0;
+			num = 0;
+		}
+		CLK_SysTickDelay(5000);
+	}
 }
